4121413c9dff9acaf918a9ff68b641f109d72aaa
Kevin Wang <kevin.wang11@outlook.com>
2021-01-04 20:58:12 -0500

    Init

---
+++ Blink/Blink/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Blink
+//
+//  Created by Kevin Wang on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
